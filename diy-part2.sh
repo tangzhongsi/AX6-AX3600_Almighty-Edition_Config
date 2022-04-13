a7582ea37852602038aa893743e0db7ae5eaf173
tangzhongsi <33077106+tangzhongsi@users.noreply.github.com>
2022-04-13 15:06:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -46,4 +46,5 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 #rm -rf ../lean/luci-theme-argon
 
 #frpc_v0.17.0
+pwd
 mv -f ../frp-Makefile package/lean/frp/Makefile
